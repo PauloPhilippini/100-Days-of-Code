b741978cbbb063127c3be0a9285a0cc3eab7eb82
Paulo Lombardi <165519041+PauloPhilippini@users.noreply.github.com>
2024-04-18 19:58:07 -0300

    Add files via upload

---
+++ 100 Days of code/Day 5/day_5_start.py
@@ -0,0 +1,13 @@
+#for with list
+video_games = ['PS1', 'PS2', 'PS3']
+
+for consoles in video_games:
+  print(consoles)
+
+#loop with range
+numbers = []
+for number in range(1, 11, 3):
+  numbers.append(number)
+  print(number)
+
+print(numbers)
