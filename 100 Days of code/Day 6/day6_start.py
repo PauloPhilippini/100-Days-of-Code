0962ecb15ad34b27a82c3737de72df1df9ff4a9c
Paulo Lombardi <165519041+PauloPhilippini@users.noreply.github.com>
2024-04-18 22:47:14 -0300

    Add files via upload

---
+++ 100 Days of code/Day 6/day6_start.py
@@ -0,0 +1,13 @@
+print('Hellow World')
+
+def my_print():
+  print('This is my print from a function')
+
+my_print()
+
+#for use
+#for itens in a list (iterable itemns):
+#  do something to each item
+
+#for number in range(1,10):
+#  print(number)
